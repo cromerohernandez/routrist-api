c3349e68ec453a1368e22861caf444c518cb5389
cromerohernandez <c.romero.hsv@gmail.com>
2020-02-17 20:46:09 +0100

    add: auth.middleware

--- config/routes.js
+++ config/routes.js
@@ -6,29 +6,31 @@ const citiesController = require('../controllers/cities.controller')
 const placesController = require('../controllers/places.controller')
 const usersController = require('../controllers/users.controller')
 
+const authMiddleware = require('../middlewares/auth.middleware')
+
 //tourists
-router.post('/tourists/new', touristsController.create)
+router.post('/tourists/new', authMiddleware.isNotAuthenticated, touristsController.create)
 router.get('/tourists/:token/validate', touristsController.validate)
-router.patch('/tourists/:id'/*:username*/, touristsController.update)
-router.delete('/tourists/:id'/*:username*/, touristsController.delete)
+router.patch('/tourists/:id'/*:username*/, authMiddleware.isAuthenticated, touristsController.update)
+router.delete('/tourists/:id'/*:username*/, authMiddleware.isAuthenticated, touristsController.delete)
 
 //cities
-router.post('/cities/new', citiesController.create)
+router.post('/cities/new', authMiddleware.isNotAuthenticated, citiesController.create)
 router.get('/cities/:token/validate', citiesController.validate)
-router.patch('/cities/:id'/*:name*/, citiesController.update)
-router.delete('/cities/:id'/*:name*/, citiesController.delete)
+router.patch('/cities/:id'/*:name*/, authMiddleware.isAuthenticated, citiesController.update)
+router.delete('/cities/:id'/*:name*/, authMiddleware.isAuthenticated, citiesController.delete)
 
 //places
-router.get('/places', placesController.list) //// => dev FILTER & SORT
-router.post('/places/new', placesController.create)
-router.patch('/places/:id', placesController.update)
-router.delete('/places/:id', placesController.delete)
-router.post('/places/:id/like', placesController.like)
+router.get('/places', authMiddleware.isAuthenticated, placesController.list) //// => dev FILTER & SORT
+router.post('/places/new', authMiddleware.isAuthenticated, placesController.create)
+router.patch('/places/:id', authMiddleware.isAuthenticated, placesController.update)
+router.delete('/places/:id', authMiddleware.isAuthenticated, placesController.delete)
+router.post('/places/:id/like', authMiddleware.isAuthenticated, placesController.like)
 
 //routes
 
 //sessions
-router.post('/login', usersController.login)
-router.post('/logout', usersController.logout)
+router.post('/login', authMiddleware.isNotAuthenticated, usersController.login)
+router.post('/logout', authMiddleware.isAuthenticated, usersController.logout)
 
 module.exports = router
